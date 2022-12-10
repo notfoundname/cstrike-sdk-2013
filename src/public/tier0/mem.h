1dcb2f6e12fed7e071c4aa5927d0b48ba59c1364
notfoundname <artem0savkin@gmail.com>
2022-12-10 17:01:05 +0700

    Integrate some dependencies
    tier0, tier2, vtf, particles, vstdlib, shaderlib

--- src/public/tier0/mem.h
+++ src/public/tier0/mem.h
@@ -13,10 +13,6 @@
 #endif
 
 #include <stddef.h>
-#ifdef LINUX
-#undef offsetof
-#define offsetof(s,m)	(size_t)&(((s *)0)->m)
-#endif
 
 #include "tier0/platform.h"
 
@@ -39,11 +35,12 @@
 //-----------------------------------------------------------------------------
 // DLL-exported methods for particular kinds of memory
 //-----------------------------------------------------------------------------
-MEM_INTERFACE void *MemAllocScratch( int nMemSize );
+MEM_INTERFACE void *MemAllocScratch(int nMemSize);
 MEM_INTERFACE void MemFreeScratch();
+MEM_INTERFACE void MemAllocOOMError(size_t nSize);
 
 #ifdef _LINUX
-MEM_INTERFACE void ZeroMemory( void *mem, size_t length );
+MEM_INTERFACE void ZeroMemory(void *mem, size_t length);
 #endif
 
 
